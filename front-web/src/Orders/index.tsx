278aabf96b4a95871a765500ccd302d292e4d9fd
Mauricio Silva Amaral <mauricio.s.amr@gmail.com>
2021-01-10 00:24:58 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,11 @@
+import Stepsheader from './StepsHeader'
+import './styles.css'
+
+function Orders(){
+    return (
+        <div className = "orders-container">
+            <Stepsheader />
+        </div>       
+    )
+}
+export default Orders;
